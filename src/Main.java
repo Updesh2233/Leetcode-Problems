2d0c3d4151785ad68eb516383affd0fecbe79e40
Updesh Yadav <168058490+Updesh2233@users.noreply.github.com>
2026-09-22 16:34:19 +0530

    Patter printing

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,17 @@
 public class Main {
     public static void main(String[] args) {//TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         System.out.println("Hello World");
+
+        int n = 8;
+
+        for(int row=1; row<=n; row++) {
+            //for each row -> n col
+            for(int col=1; col<=n; col++){
+                //enter star
+                System.out.println("*");
+            }
+            //to move next line
+            System.out.println();
+        }
     }
 }
