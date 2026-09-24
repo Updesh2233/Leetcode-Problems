5e64ff16df9adfe3bf1a3931248d7e9b0ec07675
Updesh Yadav <168058490+Updesh2233@users.noreply.github.com>
2026-09-25 01:26:04 +0530

    Patter printing

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,53 @@
+import com.sun.security.jgss.GSSUtil;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {//TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         System.out.println("Hello World");
-
+// Square Pattern
         int n = 8;
 
         for(int row=1; row<=n; row++) {
             //for each row -> n col
             for(int col=1; col<=n; col++){
                 //enter star
-                System.out.println("*");
+                System.out.print("*");
             }
             //to move next line
+            System.out.println();
+        }
+        // rectangle shape
+        int m = 3;
+        for(int row=1; row<=m; row++){
+            for(int col=1; col<=5; col++){
+                System.out.print("@ ");
+            }
+            System.out.println();
+        }
+
+        //right angle triangle
+        int p = 5;
+
+        for(int row=1; row<=p; row++){
+
+            for(int col=1; col<=row; col++) {
+                System.out.print("# ");
+            }
+        System.out.println();
+        }
+
+        // rombus
+
+        int u = 5;
+        for(int row=1; row<=u; row++){
+
+            for(int col=1; col<=u-row; col++){
+                System.out.print(" ");
+            }
+            for(int col=1; col<=u; col++){
+                System.out.print("* ");
+            }
             System.out.println();
         }
     }
